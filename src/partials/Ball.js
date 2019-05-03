2906344c3b14e05987ef6b570587acab67f0cedc
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-05-03 11:59:34 +0100

    ball bounces like a champ (off all 4 walls), starting pause function

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -30,9 +30,27 @@ export default class Ball {
     this.vx = this.direction * (6 - Math.abs(this.vy));
     }
 
-    render(mySvg){
-        let ball = document.createElementNS(SVG_NS, 'circle')
+    wallBounce() {
+        const hitTop = this.y - this.size <= 0
+        const hitBot = this.y + this.size >= this.boardHeight
 
+        const hitLeft = this.x - this.size <= 0
+        const hitRight = this.x + this.size >= this.boardWidth
+
+        if (hitLeft || hitRight) {
+            this.vx = -this.vx
+        }
+         else if (hitTop || hitBot) {
+            this.vy = -this.vy
+        }
+    }
+
+    render(mySvg){
+
+        this.x += this.vx;
+        this.y += this.vy;
+
+        let ball = document.createElementNS(SVG_NS, 'circle')
         ball.setAttributeNS(null, "r", this.size)
         ball.setAttributeNS(null, "cx", this.x)
         ball.setAttributeNS(null, "cy", this.y)
@@ -40,8 +58,6 @@ export default class Ball {
 
         mySvg.appendChild(ball)
 
-        this.x += this.vx;
-        this.y += this.vy;
+        this.wallBounce()
     }
-
 }
